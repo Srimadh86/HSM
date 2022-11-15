98660c0b6885c0e1a462aae381164a92b5cb24f9
p-vedantam <112190233+p-vedantam@users.noreply.github.com>
2022-11-15 20:20:49 +0530

    Update environment.prod.ts

--- HMSHost-Frontend-Repo/src/environments/environment.prod.ts
+++ HMSHost-Frontend-Repo/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
   production: true
+  masterData:`http://20.244.4.72:8000/api/webservices/masters/list`,
+ users:'https://mocki.io/v1/f602aa94-ad9e-4bc5-86d9-49c17b8b5f51',
+  // users: `http://20.244.4.72:3000/users`,
+  recognition: `http://20.244.4.72:3000/recognitions`,
+  gratitude: `http://20.244.4.72:3000/gratitudes`,
+  categoryList: 'http://20.244.4.72:3000/recognition-categories',
+  baseUrl:'http://20.244.4.72:8000/api/webservices/',
+
 };

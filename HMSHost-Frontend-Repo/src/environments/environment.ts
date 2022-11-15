7dfa9a206510537455cb8471131fba16258d0947
p-vedantam <112190233+p-vedantam@users.noreply.github.com>
2022-11-15 21:25:21 +0530

    Update environment.ts

--- HMSHost-Frontend-Repo/src/environments/environment.ts
+++ HMSHost-Frontend-Repo/src/environments/environment.ts
@@ -4,13 +4,13 @@
 
 export const environment = {
   production: false,
-masterData:`http://localhost:8000/api/webservices/masters/list`,
+masterData:`http://20.244.4.72:8000/api/webservices/masters/list`,
  users:'https://mocki.io/v1/f602aa94-ad9e-4bc5-86d9-49c17b8b5f51',
   // users: `http://localhost:3000/users`,
-  recognition: `http://localhost:3000/recognitions`,
-  gratitude: `http://localhost:3000/gratitudes`,
-  categoryList: 'http://localhost:3000/recognition-categories',
-  baseUrl:'http://localhost:8000/api/webservices/',
+  recognition: `http://20.244.4.72:8000/recognitions`,
+  gratitude: `http://20.244.4.72:8000/gratitudes`,
+  categoryList: 'http://20.244.4.72:8000/recognition-categories',
+  baseUrl:'http://20.244.4.72:8000/api/webservices/',
 
 
 };
